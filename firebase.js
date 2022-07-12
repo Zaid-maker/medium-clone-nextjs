3f3cae1c0ec213b65fab09bf988b7e7612de5c05
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-07-12 11:19:52 +0000

    firebase wuth setuped

--- firebase.js
+++ firebase.js
@@ -4,12 +4,13 @@ import { GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDG0orlR-jfXS6L_qkbsAC8Uizn3J02Nf0",
+  authDomain: "medium-clone-nextjs.firebaseapp.com",
+  projectId: "medium-clone-nextjs",
+  storageBucket: "medium-clone-nextjs.appspot.com",
+  messagingSenderId: "215501689111",
+  appId: "1:215501689111:web:37f09f2c3143c66c256752",
+  measurementId: "G-5GJM5P6JCD",
 };
 
 const app = initializeApp(firebaseConfig);
