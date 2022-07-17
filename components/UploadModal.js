8e6263be2a74ad8afbf002c2887fa82e1a7b3157
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-07-17 18:20:46 +0000

    complete modal

--- components/UploadModal.js
+++ components/UploadModal.js
@@ -1,4 +1,8 @@
-import React from "react";
+import { useState, useEffect, useContext } from "react";
+import { useRouter } from "next/router";
+import { MediumContext } from "../context/MediumContext";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
 
 const styles = {
   wrapper: `w-[70rem] h-[50rem] flex flex-col justify-start items-center gap-[1rem] p-[1rem] font-mediumSerif overflow-scroll`,
@@ -12,9 +16,89 @@ const styles = {
 };
 
 const UploadModal = () => {
+  const router = useRouter();
+  const { user } = useContext(MediumContext);
+
+  const [title, setTitle] = useState("");
+  const [brief, setBrief] = useState("");
+  const [category, setCategory] = useState("");
+  const [postLength, setPostLength] = useState("");
+  const [bannerImage, setBannerImage] = useState("");
+  const [body, setBody] = useState("");
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>Upload a Post</div>
+      <div className={styles.smallField}>
+        <span className={styles.fieldTitle}>Title</span>
+        <span className={styles.inputContainer}>
+          <input
+            className={styles.inputField}
+            type="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </span>
+      </div>
+      <div className={styles.smallField}>
+        <span className={styles.fieldTitle}>Brief</span>
+        <span className={styles.inputContainer}>
+          <input
+            className={styles.inputField}
+            type="text"
+            value={brief}
+            onChange={(event) => setBrief(event.target.value)}
+          />
+        </span>
+      </div>
+      <div className={styles.smallField}>
+        <span className={styles.fieldTitle}>Banner Image URL</span>
+        <span className={styles.inputContainer}>
+          <input
+            className={styles.inputField}
+            type="text"
+            value={bannerImage}
+            onChange={(event) => setBannerImage(event.target.value)}
+          />
+        </span>
+      </div>
+      <div className={styles.smallField}>
+        <span className={styles.fieldTitle}>Category</span>
+        <span className={styles.inputContainer}>
+          <input
+            className={styles.inputField}
+            type="text"
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+          />
+        </span>
+      </div>
+      <div className={styles.smallField}>
+        <span className={styles.fieldTitle}>
+          Estimated Read Length (in minutes)
+        </span>
+        <span className={styles.inputContainer}>
+          <input
+            className={styles.inputField}
+            type="text"
+            value={postLength}
+            onChange={(event) => setPostLength(event.target.value)}
+          />
+        </span>
+      </div>
+      <div className={styles.largeField}>
+        <span className={styles.fieldTitle}>Article Text</span>
+        <span className={styles.inputContainer}>
+          <textarea
+            className={styles.inputField}
+            value={body}
+            rows={12}
+            onChange={(event) => setBody(event.target.value)}
+          />
+        </span>
+      </div>
+
+      <button className={styles.accentedButton}>Submit</button>
     </div>
   );
 };
