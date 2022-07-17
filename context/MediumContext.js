b755bbe66dada9ba33774c5497da1daf30ee9571
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-07-17 18:04:08 +0000

    useeffects

--- context/MediumContext.js
+++ context/MediumContext.js
@@ -10,6 +10,52 @@ export const MediumProvider = ({ children }) => {
   const [allUsers, setAllUsers] = useState([]);
   const [allPosts, setAllPosts] = useState([]);
 
+  useEffect(() => {
+    const getAllUsers = async () => {
+      const querySnapshot = await getDocs(collection(db, "users"));
+
+      setAllUsers(
+        querySnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            data: {
+              ...doc.data(),
+            },
+          };
+        })
+      );
+    };
+
+    getAllUsers();
+  }, [user]);
+
+  useEffect(() => {
+    const getAllPosts = async () => {
+      const querySnapshot = await getDocs(collection(db, "posts"));
+
+      setAllPosts(
+        querySnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            data: {
+              body: doc.data().body,
+              brief: doc.data().brief,
+              category: doc.data().category,
+              postLength: doc.data().postLength,
+              bannerImage: doc.data().bannerImage,
+              title: doc.data().title,
+              comments: doc.data().comments,
+              postedOn: doc.data().postedOn.toDate(),
+              author: doc.data().author,
+            },
+          };
+        })
+      );
+    };
+
+    getAllPosts();
+  }, []);
+
   const saveUser = async (user) => {
     await setDoc(doc(db, "users", user.email), {
       email: user.email,
